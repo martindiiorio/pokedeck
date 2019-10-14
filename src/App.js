d06a2d19ddcf9eefd85e442bdcdc6c8406de0ca3
Martin Di iorio <martin.diiorio@rga.com>
2019-10-14 17:58:43 +0200

    adding Dashboard component & fix some structure concepts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
-import Header from './components/Header/Header';
+import Header from './components/layout/Header';
+import Dashboard from './components/layout/Dashboard';
 import './App.scss';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <Dashboard />
     </ Fragment>
   );
 }
---
+++ src/components/pokemon/PokemonCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class PokemonCard extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="card">
+          <div className="card-header">
+            <h1>Pokemon Card Test</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
