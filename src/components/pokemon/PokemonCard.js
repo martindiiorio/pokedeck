754b86f308d26505d5b1faf3482f48c2af5db175
Martin Di iorio <martin.diiorio@rga.com>
2019-10-15 10:12:01 +0200

    Pokeapi requests done & rendering a list of 151 pokemons

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
 
 export default class PokemonCard extends Component {
+  state = {
+    name: '',
+    imageUrl: '',
+    pokemonIndex: ''
+  }
+
+  componentDidMount() {
+    const { name, url } = this.props;
+    const pokemonIndex = url.split('/')[url.split('/').length -2];
+    const imageUrl = `http://pokestadium.com/sprites/xy/${name}.gif`;
+
+    this.setState({
+      name,
+      imageUrl,
+      pokemonIndex
+    })
+  }
+
   render() {
+
     return (
       <div className="container">
         <div className="card">
           <div className="card-header">
-            <h1>Pokemon Card Test</h1>
+            <img src={this.state.imageUrl} alt={this.state.name + 'image'} />
+          </div>
+          <div className="card-body">
+            <h1>{this.state.name}</h1>
           </div>
         </div>
       </div>
--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -1,12 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import axios from 'axios';
+
 import PokemonCard from './PokemonCard';
 
 export default class PokemonList extends Component {
+  state = {
+    url: 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151',
+    pokemon: null
+  }
+
+  async componentDidMount() {
+    const res = await axios.get(this.state.url);
+    this.setState({ pokemon: res.data['results'] })
+  } 
+
   render() {
     return (
-      <div>
-        <PokemonCard />
-      </div>
+      <Fragment>
+        {this.state.pokemon ? (
+          <div>
+            {this.state.pokemon.map(pokemon => (
+              <PokemonCard  
+                key={pokemon.name}
+                name={pokemon.name}
+                url={pokemon.url}
+              />
+            ))}
+          </div>
+        ) : (
+          <h1>Loading Pokemon</h1>
+        )}
+      </Fragment> 
     )
   }
 }
